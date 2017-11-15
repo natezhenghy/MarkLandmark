3ec71c7256d76451462d05bfebbf23da4319395b
natezhenghy <natezhenghy@outlook.com>
2017-11-15 11:46:07 +0800

    Initial commit

---
+++ LandmarksLabeler/Views/View.xaml.cs
@@ -0,0 +1,29 @@
+﻿using LandmarksLabeler.ViewModels;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace LandmarksLabeler.Views
+{
+    public sealed partial class View : Page
+    {
+        private readonly ViewModel _viewModel = new ViewModel();
+
+        public View()
+        {
+            InitializeComponent();
+            DataContext = _viewModel;
+        }
+
+        private void LM_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            var lm = (Ellipse)sender;
+            var trans = (CompositeTransform)lm.RenderTransform;
+
+            if (trans == null) return;
+            trans.TranslateX += e.Delta.Translation.X;
+            trans.TranslateY += e.Delta.Translation.Y;
+        }
+    }
+}
